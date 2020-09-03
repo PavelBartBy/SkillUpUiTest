e850c29608bd0426e9572bdc5ba6ed2de13c6b50
PavelBartBy <wildseller91@gmail.com>
2020-09-03 20:59:33 +0300

    ExercisesSkillUp 1.Sum of even positive elements of array 2.Max element 3.Min element 4.Elements which less than the arithmetic mean 5.Quick sort algorithm

---
+++ src/main/java/basic_algorithm/OperationsWithArray.java
@@ -0,0 +1,71 @@
+package basic_algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperationsWithArray {
+
+    public static int sumEvenPositive (int[] array) {
+
+        int sum = 0;
+        for (int j : array) {
+            if (j >= 0) {
+                if (j % 2 == 0) {
+                    sum = sum + j;
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static int maxElement(int[] array){
+        int max = 0;
+        for (int i:array){
+            if (i>=max){
+                max=i;
+            }
+        }
+        return max;
+    }
+
+    public static int minElement(int[] array){
+        int min = 0;
+        for (int i:array){
+            if (i<min){
+                min=i;
+            }
+        }
+        return min;
+    }
+
+    public static List<Integer> lowElements (int[] array){
+
+        int sum = 0;
+        for (int i:array){
+            sum=sum+i;
+        }
+        double amean=(double)sum/array.length;
+        System.out.println(sum+" and "+ array.length);
+        System.out.println("The arithmetic mean is " + amean);
+        List<Integer> lowelements = new ArrayList<Integer>();
+        for (int j:array){
+            if(j<amean){
+                lowelements.add(j);
+            }
+        }
+        return lowelements;
+    }
+// for test:
+//        int[] x = { 8, 0, 3, -6, 7, -44, 3, 7, -13, -1, -10, 10, 12, -3 };
+//        System.out.println(Arrays.toString(x));
+//        int s = SumEvenPositive(x);
+//        System.out.println("Sum of even positive numbers is "+ s);
+//    int max = maxElement(x);
+//        System.out.println("Max Element is " + max);
+//    int min = minElement(x);
+//        System.out.println("Min Element is " + min);
+//    List<Integer> lowelements = new ArrayList<Integer>();
+//    lowelements = lowElements(x);
+//        System.out.println("Elements which less than the arithmetic mean " + lowelements);
+
+}
